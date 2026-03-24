b6ed26a84d6a076883c07c1455e5ecfc27dbc5b4
Colin Menuchi <cdmenuchi@gmail.com>
2026-03-24 00:01:28 -0400

    They fly now? THEY FLY NOW

--- portfolio-website/src/components/Hero.jsx
+++ portfolio-website/src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
         <img className="hero-photo" src="src/assets/selfPortrait.jpeg" alt="Portrait Not Found."></img>
       </div>
       <h1 className="hero-name">Colin Menuchi</h1>
-      <p className="hero-title">Aspiring Software Engineer</p>
+      <p className="hero-title">Young & Fresh Software Engineer</p>
       <p className="hero-bio">
         Hi! I'm a software engineering student with a passion for building cool things —
         whether that's training AI to escape mazes, recreating history as a fighting game,
