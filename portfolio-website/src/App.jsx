17153bbac85d62a01dd5902ecf0fb81112bbde20
Colin Menuchi <cdmenuchi@gmail.com>
2026-03-25 22:30:41 -0400

    Added footer with message about copyright

--- portfolio-website/src/App.jsx
+++ portfolio-website/src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Experience from './components/Experience'
 import Projects from './components/Projects'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Experience />
         <Projects />
       </div>
+      <Footer />
     </>
   )
 }
---
+++ portfolio-website/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>© 2026 Colin Menuchi. All Rights Reserved.</p>
+    </footer>
+  )
+}
+
+export default Footer
