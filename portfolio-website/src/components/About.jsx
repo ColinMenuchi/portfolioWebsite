429fc379e4b5a98eb84ab0371983f0a9fa8c30d7
Colin Menuchi <cdmenuchi@gmail.com>
2026-03-26 14:18:28 -0400

    Slightly modified about me paragraph

--- portfolio-website/src/components/About.jsx
+++ portfolio-website/src/components/About.jsx
@@ -92,7 +92,9 @@ const About = () => {
     <section id="about" className="about-section">
       <h2 className="about-heading">About Me</h2>
       <p className="about-bio">
-        I'm Colin Menuchi, a software engineering student with a love for building things that matter.
+        Hi, I'm Colin! I'm a thrid-year student studying Computer Science with a concentration in
+        AI at Northeastern University, and an incoming software engineering intern at Fidelity Investments!
+        student with a love for building things that matter.
         Whether I'm designing AI systems, crafting full-stack applications, or diving into a new
         programming challenge, I bring curiosity and creativity to everything I do. When I'm not
         staring at a screen, you can find me exploring the world around me and staying inspired by
