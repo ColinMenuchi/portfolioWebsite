c51ff022f82cc159107c3bef7ec076521b9f1dbe
Colin Menuchi <cdmenuchi@gmail.com>
2026-03-27 23:10:33 -0400

    edited my intro hero statement

--- portfolio-website/src/components/Hero.jsx
+++ portfolio-website/src/components/Hero.jsx
@@ -11,11 +11,12 @@ const Hero = () => {
         <img className="hero-photo" src="src/assets/selfPortrait.jpeg" alt="Portrait Not Found."></img>
       </div>
       <h1 className="hero-name">Colin Menuchi</h1>
-      <p className="hero-title">Young & Fresh Software Engineer</p>
+      <p className="hero-title">Incoming Software Engineering Intern at Fidelity Investments</p>
       <p className="hero-bio">
-        Hi! I'm a software engineering student with a passion for building cool things —
-        whether that's training AI to escape mazes, recreating history as a fighting game,
-        or helping people remember what's in their fridge. Welcome to my corner of the internet!
+        Hi! I'm a Computer Science student at Northeastern University with a passion for building impactful things
+        in my community. Whether that's a website for my school's board game club, satirizing modern US politics as
+        a Pokémon game, or helping people remember what's in their fridge, I always strive to enhance the lives
+        of the people around me! Welcome to my corner of the internet, I hope you enjoy your stay!
       </p>
       <div className="hero-socials">
         {socials.map((s) => (
